f23d88baafd84f3f82fe2e272fb073a332ad672d
Md. Majaharul Islam <123297124+Majaharulsoft@users.noreply.github.com>
2023-11-02 13:29:53 +0600

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Majaharul Islam Rana</strong>
+                <strong className="main-name"> Majaharul Islam MIR</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
