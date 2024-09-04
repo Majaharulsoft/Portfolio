a34ca804769b935d458b7a63bfd5388ac567de63
Md. Majaharul Islam <123297124+Majaharulsoft@users.noreply.github.com>
2024-09-04 16:43:07 +0600

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -7,7 +7,7 @@ function AboutCard() {
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
+          <p style={{ textAlign: "justify",letterSpacing: -1px; }}>
             Hi Everyone, I am <span className="purple">Majaharul Islam Rana </span>
             from <span className="purple"> khilkhet, Dhaka, Bangladesh.</span>
             <br />I completed my graduation in Computer Science and Engineering from Northern University Bangladesh this year.
